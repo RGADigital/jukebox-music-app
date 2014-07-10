48bdcc97ae1ce2c39286ddc1d4c65b3c3803064e
Zdaxia <zouziqu@gmail.com>
2014-07-10 17:37:38 -0400

    update DOM and events

--- music-app-rdio-v1.3/my_modules/rdio_consumer_credentials.js
+++
@@ -1,4 +0,0 @@
-// You can get these by signing up for a developer account at:
-// http://developer.rdio.com/
-exports.RDIO_CONSUMER_KEY = "j4xqewdmwsuff3ah76rc8qzh"
-exports.RDIO_CONSUMER_SECRET = "X5QF7HvN2N"
