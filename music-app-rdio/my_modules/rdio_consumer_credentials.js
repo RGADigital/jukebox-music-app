199d12dfaa7005c830eb303403af5504c7b20d45
hous <hous27@gmail.com>
2016-04-04 10:37:43 -0400

    Remove creds from repo.

--- music-app-rdio/my_modules/rdio_consumer_credentials.js
+++ music-app-rdio/my_modules/rdio_consumer_credentials.js
@@ -1,3 +1,3 @@
 
-exports.RDIO_CONSUMER_KEY = "cttgh283ffhhxqpekax52qsv"
-exports.RDIO_CONSUMER_SECRET = "v4Pxa6Yvy4"
+exports.RDIO_CONSUMER_KEY = "REPLACE_ME"
+exports.RDIO_CONSUMER_SECRET = "REPLACE_ME"
